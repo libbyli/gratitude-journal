1a9c26e89dc135babdc33518305dd8fdd6741788
Elizabeth Li <sincerelylibby@gmail.com>
2018-07-08 11:05:14 -0700

    Fix server logic for better error handling..

--- server/index.js
+++ server/index.js
@@ -11,30 +11,42 @@ app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/entries', (req, res) => {
   db.retrieve((err, results) => {
-    if (err) { res.status(500).send('Error in retrieving entries'); }
-    res.status(200).json(results);
+    if (err) {
+      res.status(500).send('Error in retrieving entries');
+    } else {
+      res.status(200).json(results);
+    }
   });
 });
 
 app.get('/users/:username', (req, res) => {
   let username = req.params.username;
   db.retrieveUser(username, (err, results) => {
-    if (err) { res.status(500).send('Error in retrieving user'); }
-    res.status(200).json(results);
+    if (err) {
+      res.status(500).send('Error in retrieving user');
+    } else {
+      res.status(200).json(results);
+    }
   });
 });
 
 app.post('/entries', (req, res) => {
   db.addEntry(req, (err, results) => {
-    if (err) { res.status(500).send('Error in posting entry'); }
-    res.status(201).json(results);
+    if (err) {
+      res.status(500).send('Error in posting entry');
+    } else {
+      res.status(201).json(results);
+    }
   });
 });
 
 app.post('/users', (req, res) => {
   db.addUser(req, (err, results) => {
-    if (err) { res.status(500).send('Error in adding user'); }
-    res.status(201).json(results);
+    if (err) {
+      res.status(500).send('Error in adding user');
+    } else {
+      res.status(201).json(results);
+    }
   });
 });
 
